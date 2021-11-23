9f60133fdef504a6d65eddcf85b6eb9b24e7a4b6
m210 <m210-2007@mail.ru>
2021-11-23 08:53:48 +0300

    Bug fix :)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,13 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-        task1(1.0);
+    private static final int iOS = 0;
+    private static final int AndroidOS = 1;
 
-        task2(AndroidOS, 2020);
+    public static void main(String[] args) {
+        task1(AndroidOS);
+
+        task2(iOS, 2012);
 
         task3(2022);
 
@@ -14,48 +17,56 @@ public class Main {
         task5(12);
     }
 
-    private static final int iOS = 0;
-    private static final int AndroidOS = 1;
-
-    public static void task1(Object clientOS) {
-        // Объявите переменную clientOS, которая равна 0 или 1.0 — iOS, 1 — Android.
-
-        if((clientOS instanceof Double && (double) clientOS == 1.0) || (clientOS instanceof Integer && (int) clientOS == iOS))
+    public static void task1(int clientOS) {
+        if(clientOS == iOS)
             System.out.println("Установите версию приложения для iOS по ссылке");
-        else if(clientOS instanceof Integer && (int) clientOS == AndroidOS)
+        else if(clientOS == AndroidOS)
             System.out.println("Установите версию приложения для Android по ссылке");
         else System.out.println("Неизвестное устройство");
-
-//        var clientOS = 1.0;
-//        if(clientOS == 1) {
-//            System.out.println("Установите версию приложения для Android по ссылке");
-//        } else if(clientOS == 1.0 || clientOS == 0) {
-//            System.out.println("Установите версию приложения для iOS по ссылке");
-//        } else System.out.println("Неизвестное устройство");
     }
 
     public static void task2(int OS, int clientDeviceYear) {
-        if(clientDeviceYear >= 2015 && OS == AndroidOS)
-            System.out.println("Установите версию приложения для Android по ссылке");
-        else if(clientDeviceYear >= 2015 && OS == iOS)
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        else if(OS == AndroidOS)
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        else System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+//        if(clientDeviceYear >= 2015 && OS == AndroidOS)
+//            System.out.println("Установите версию приложения для Android по ссылке");
+//        else if(clientDeviceYear >= 2015 && OS == iOS)
+//            System.out.println("Установите версию приложения для iOS по ссылке");
+//        else if(OS == AndroidOS)
+//            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+//        else System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+
+        if(OS != iOS && OS != AndroidOS) {
+            System.out.println("Неизвестное устройство");
+            return;
+        }
+
+        if(clientDeviceYear < 2015) {
+            if(OS == iOS)
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            else if(OS == AndroidOS)
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else task1(OS);
     }
 
     public static void task3(int year) {
-        if((year % 4) == 0 || ((year % 100) == 0 && (year % 400) == 0)) {
+        if(((year % 4) == 0 && (year % 100) != 0) || (year % 400) == 0) {
             System.out.println(year + " год является високосным");
         } else System.out.println(year + " год не является високосным");
     }
 
     public static void task4(int deliveryDistance) {
-        int extraDistance = deliveryDistance - 20;
-        int days = 1 + extraDistance / 40;
-        if((extraDistance % 40) != 0)
-            days++;
-
+       int days;
+       if(deliveryDistance < 20)
+           days = 1;
+       else if(deliveryDistance >= 20 && deliveryDistance < 60)
+           days = 2;
+       else if(deliveryDistance >= 60 && deliveryDistance < 100)
+           days = 3;
+       else {
+           int extraDistance = deliveryDistance - 19; // отнимать 19 или 20, зависит, нужно ли захватывать 20й км для добавления 1 дня.
+           days = 1 + extraDistance / 40;
+           if(extraDistance > 0 && (extraDistance % 40) != 0) // Эта формула теперь будет работать и для дистанции < 20км, поэтому остальные условия избыточныы
+               days++;
+       }
        System.out.println("Потребуется дней: " + days);
     }
 
